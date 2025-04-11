59fc5fd603af5f83ca68158bbb7054593c156be4
aaribanwar <aaribactivism@gmail.com>
2025-04-11 12:05:21 +0530

    revamp backend

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,38 +1,12 @@
-const mongoose = require("mongoose");
+// models/user.js
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
 
-async function main(){
-    mongoose.connect("mongodb://127.0.0.1:27017/miniProject");
-}
-
-await main();
-
-const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    deviceId: {
-        type: String
-    },
-    notificationEnabled: {
-        type: Boolean,
-        default: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    }
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true } // eventually hashed
 });
 
-const User = mongoose.model("User",UserSchema);
-
-module.exports(User);
+const User = model("User", userSchema);
+export default User;
