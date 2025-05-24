934242cedfcb315b7a9006e3e7d73f14e251d2f8
kshitija-kumar <kshitija0211@gmail.com>
2025-05-24 18:30:34 +0530

    Removed nested git repo, added frontend code as regular files

--- frontend/src/pages/Logs.jsx
+++
@@ -1,8 +0,0 @@
-const Logs = () => (
-    <div className="dashboard">
-      <h1>📜 Alert Logs</h1>
-      <p>This page will list all the previous alerts and sounds detected.</p>
-    </div>
-  );
-  export default Logs;
-  
